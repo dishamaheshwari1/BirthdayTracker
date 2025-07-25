607de4df613a578c7f6e24abdfe2194f4ef27009
Disha Maheshwari <disha.maheshwari@icloud.com>
2025-07-25 12:58:35 -0400

    Initial Commit

---
+++ BirthdayTracker/BirthdayTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayTrackerApp.swift
+//  BirthdayTracker
+//
+//  Created by Disha Maheshwari on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirthdayTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BirthdayTracker
+//
+//  Created by Disha Maheshwari on 7/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
